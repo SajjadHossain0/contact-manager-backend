2262e424db33c2bbf844265798df4db9d8b12330
mdsaj <mdsajjadhossain614@gmail.com>
2024-10-31 00:54:17 +0600

    new commit

---
+++ src/main/java/com/contact_manager/Entities/User.java
@@ -0,0 +1,25 @@
+package com.contact_manager.Entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    private String image;
+    private String name;
+    private String email;
+    private String password;
+}
