b13580bd56acab5607facfa259bc5a9b001b5737
mdsaj <mdsajjadhossain614@gmail.com>
2024-11-03 00:04:27 +0600

    new commit

--- src/main/java/com/contact_manager/Repository/ContactRepository.java
+++ src/main/java/com/contact_manager/Repository/ContactRepository.java
@@ -3,6 +3,8 @@ package com.contact_manager.Repository;
 import com.contact_manager.Entities.Contact;
 import com.contact_manager.Entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
